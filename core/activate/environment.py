356d68f376667c92d6caf5c8a2e4f4b9c985e227
jenia petrusenko <medvijenia@icloud.com>
2022-09-01 00:22:30 +0300

    adding freeze

--- core/activate/environment.py
+++ core/activate/environment.py
@@ -9,5 +9,9 @@ def create() -> None:
     system(fr"python -m venv { config.read('path', 'project') }/venv")
 
 
+def save_requirements() -> None:
+    system(fr"pip freeze { config.read('path', 'project') }/venv")
+
+
 if __name__ == '__main__':
-    create()
+    ...
