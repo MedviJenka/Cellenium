9bd8aee2645e025a202753330321a35175595734
Jenia Petrusenko <evgenyp@audiocodes.com>
2022-10-31 18:44:55 +0200

    logs

--- core/components/driver/run_tests.py
+++ core/components/driver/run_tests.py
@@ -2,26 +2,11 @@ from dataclasses import dataclass
 from core.components.logs.log_generator import Log
 
 
-class DummyClass:
-
-    def test1(self) -> None: ...
-    def test2(self) -> None: ...
-    def test3(self) -> None: ...
-
-
 @dataclass
 class RunTests(Log):
 
+    @Log.get_log()
     def start(self, class_name: object, methods: list[str]) -> None:
         for each_method in methods:
             getattr(class_name, each_method)()
-        self.get_log()
-
-
-def main() -> None:
-    run = RunTests()
-    run.start(DummyClass(), ['test1', 'test2', 'test3'])
-
-
-if __name__ == "__main__":
-    main()
+            print(f'test steps: {each_method}')
