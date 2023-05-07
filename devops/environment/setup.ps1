dde902c39b9378dae1b4aa397f3ec28c0d027ede
jenia petrusenko <medvijenia@icloud.com>
2023-05-07 03:36:00 +0300

    vm

--- devops/environment/setup.ps1
+++ devops/environment/setup.ps1
@@ -2,24 +2,19 @@ $packages = 'virtualbox', 'vagrant', 'awscli', 'sublimetext3.app'
 
 
 if (Test-Path -Path "$env:ProgramData\Chocolatey") {
-<<<<<<< HEAD
+
     foreach ($packageName in $packages) {
-=======
-  # DoYourPackageInstallStuff
-  foreach ($packageName in $packages)
-    {
->>>>>>> ae4cbfe556aa68ef93ce728ef91ae8b249147202
         choco install $packageName -y
     }
+
 }
 
 else {
-  # InstallChoco
+
     Set-ExecutionPolicy Bypass -Scope Process -Force;
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
     Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-  # DoYourPackageInstallStuff
     foreach ($packageName in $packages) {
         choco install $packageName -y
     }
