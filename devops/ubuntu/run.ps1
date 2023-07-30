5e07098e23e6969531cd7b5eb5f757c7440182b3
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2023-07-30 13:57:59 +0300

    remove devops

--- devops/ubuntu/run.ps1
+++
@@ -1,9 +0,0 @@
-set-location -path "C:\Users\evgenyp\Cellenium\devops\ubuntu\"
-
-# aws configure
-
-$commands = @('fmt', 'validate', 'plan', 'init', 'apply')
-foreach($command in $commands) {
-    write-host "executing terraform $command"
-    terraform $command
-}
