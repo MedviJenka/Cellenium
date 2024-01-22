1ea79caef34487aa2bedf2c16d4c41da759e1846
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2024-01-22 10:51:11 +0200

    adding api engine to engine.py

--- core/infrastructure/driver/engine.py
+++ core/infrastructure/driver/engine.py
@@ -1,3 +1,5 @@
+import requests
+import warnings
 from os import system
 from selenium import webdriver
 from dataclasses import dataclass
@@ -14,8 +16,11 @@ from core.infrastructure.modules.methods import *
 from core.infrastructure.modules.reader import *
 from allure_commons.types import AttachmentType
 from browsermobproxy import Server
+from typing import Optional
+from core.infrastructure.api.constants import Authorization
 
 
+warnings.filterwarnings('ignore')
 log = Logger()
 
 
@@ -233,3 +238,62 @@ class DriverEngine(DriverManager):
         for each in local_storage:
             for key, value in each.items():
                 self.driver.execute_script(f"window.localStorage.setItem('{key}', '{value}')")
+
+
+@dataclass
+class APIEngine:
+
+    """
+    class_params: base_url ........................... A string representing the base URL for the HTTP requests.
+
+    class_params: status_code ........................ A boolean, if True, returns response status code.
+
+    :params: query_path (required) ................... A string representing the endpoint.
+                                                       path to be appended to the base URL.
+    :params: headers (optional) ...................... A dictionary containing the headers to be sent with the request.
+
+    :params (optional) ............................... A dictionary containing the query parameters.
+                                                       to be sent with the request.
+    :params: verify (optional) ....................... A boolean indicating whether to verify the SSL certificate.
+
+    :returns: response in a json format
+    """
+
+    base_url: str = ''
+    status_code: bool = False
+
+    def get_request(self, query_path: str, params=Optional[dict], verify=False) -> any:
+        url = f'{self.base_url}/{query_path}'
+        response = requests.get(url, headers=Authorization.HEADERS, params=params, verify=verify)
+        if self.status_code:
+            return f'response: {response.status_code}'
+
+        log.level.debug(f'sent {response} with status code: {response.status_code}')
+        return response.json()
+
+    def post_request(self, query_path: str, body: dict[str], params=Optional[dict], verify=False) -> dict | int:
+        url = f'{self.base_url}/{query_path}'
+        response = requests.post(url, headers=Authorization.HEADERS, json=body, params=params, verify=verify)
+        if self.status_code:
+            return response.status_code
+
+        log.level.dubug(f'sent {response} with status code: {response.status_code}')
+        return response.json()
+
+    def put_request(self, query_path: str, body: dict[str], params=Optional[dict], verify=False) -> dict | int:
+        url = f'{self.base_url}/{query_path}'
+        response = requests.put(url, headers=Authorization.HEADERS, json=body, params=params, verify=verify)
+        if self.status_code:
+            return response.status_code
+
+        log.level.debug(f'sent {response} with status code: {response.status_code}')
+        return response.json()
+
+    def delete_request(self, query_path: str, params=Optional[dict], verify=False) -> dict | int:
+        url = f'{self.base_url}/{query_path}'
+        response = requests.delete(url, headers=Authorization.HEADERS, params=params, verify=verify)
+        if self.status_code:
+            return response.status_code
+
+        log.level.debug(f'sent {response} with status code: {response.status_code}')
+        return response.json()
--- tests/google/dummy.py
+++ tests/google/dummy.py
@@ -8,7 +8,7 @@ class TestTitle:
     def test_web(self) -> None:
         self.engine.get_web("https://www.google.com")
         title = self.engine.driver.title
-        assert title == 'Google'
-        self.engine.get_element('search').send_keys('google')
+        self.engine.get_element('search').send_keys('cats')
         self.engine.get_element('button').click()
         self.engine.teardown()
+        assert title == 'Google'
